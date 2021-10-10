c49c3bbd0c7fcf21e03d37730915cdbdc4abbe54
jailor123 <91145870+jailor123@users.noreply.github.com>
2021-10-10 20:46:35 +0530

    using_switch_stmt.cs

---
+++ using_switch.cs
@@ -0,0 +1,39 @@
+using System
+  
+  
+    public static void Main(String[] args)
+    {
+        int item = 6;
+        switch (item) {
+  
+        case 1:
+            Console.WriteLine("one");
+            break;
+  
+        case 2:
+            Console.WriteLine("two");
+            break;
+  
+        case 3:
+            Console.WriteLine("three");
+            break;
+  
+        case 4:
+            Console.WriteLine("four");
+            break;
+             
+         case 5:
+            Console.WriteLine("five");
+            break;
+             
+          case 6:
+            Console.WriteLine("six");
+            break;
+            
+        default:
+            Console.WriteLine("No match found");
+            break;
+        }
+    }
+}
+Output:
