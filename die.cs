9ea649e36610d60fec36c943b9cef6aa75a5a29d
jailor123 <91145870+jailor123@users.noreply.github.com>
2021-10-10 19:18:57 +0530

    playroldie.cs

---
+++ die.cs
@@ -0,0 +1,16 @@
+using System;
+class RollingDie;
+{
+    private Random random;
+    private int sidesCount;
+
+    public RollingDie()
+    {
+        sidesCount = 6;
+        random = new Random();
+    }
+    for (int i = 0; i < 6; i++)
+    Console.Write(sixSided.Roll() + " ");
+     Console.ReadKey();
+     
+     }
